9a336053027ce273d2b15d099057eb622644ce02
Kuban <kubanmuit@gmail.com>
2023-03-07 11:39:03 +0600

    Initial commit

---
+++ src/main/java/peaksoft/enums/HouseType.java
@@ -0,0 +1,5 @@
+package enums;/**
+    @created 07.03.2023   
+    @author  :ЛОКИ Kelsivbekov 
+*/public enum HouseType {
+}
---
+++ src/main/java/peaksoft/models/Feedback.java
@@ -0,0 +1,5 @@
+package models;/**
+    @created 07.03.2023   
+    @author  :ЛОКИ Kelsivbekov 
+*/public class Feedback {
+}
